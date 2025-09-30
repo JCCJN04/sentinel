77baa8b273020d2ed0777fd1d47f72f782648c61
Juan Carlos Mendoza Castillo <mendozacastillo387@gmail.com>
2025-09-29 22:00:23 -0600

    cambios y se anadio archivo backup

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -16,7 +16,7 @@ export function Sidebar({ className }: { className?: string }) {
         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/dashboard" className="flex items-center gap-2 font-semibold">
             <Shield className="h-6 w-6 text-primary" />
-            <span className="">Sentinel</span>
+            <span className="">ZYRA</span>
           </Link>
         </div>
         <div className="flex-1 overflow-y-auto">
